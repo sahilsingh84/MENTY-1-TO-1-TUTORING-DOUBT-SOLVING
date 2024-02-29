1af33df03f6237fdb8b2391ba900d7c1b2c8379a
sahilsingh84 <singhsahil840060@gmail.com>
2024-02-29 20:36:50 +0530

    add schema

---
+++ menty/Server/models/RatingAndReview.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose");
+
+
+const ratingAndReviewSchema=new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    rating:{
+        type:Number,
+    },
+    review:{
+        type:String
+    }
+})
