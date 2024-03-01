3c7ecc023b32b01bf11c47ac0a998b3fe113ec3d
sahilsingh84 <singhsahil840060@gmail.com>
2024-03-01 16:25:42 +0530

    Home section1

--- menty/src/pages/Home.jsx
+++ menty/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import BannerVideo from '../assets/banner.mp4'
 import Button from '../components/common/Button'
+import { TypeAnimation } from 'react-type-animation'
+import { Fade } from 'react-awesome-reveal'
+
 
 
 const Home = () => {
@@ -9,11 +12,26 @@ const Home = () => {
         <div className='section1'>
             <div className='setion1InitialDiv'>
             <div className='section1text'>
-                <div className='webTitle'>MENTY:1-TO-1 TUTORING & DOUBT SOLVING</div>
+                <div className='webTitle'>MENTY:
+                <TypeAnimation
+                    sequence={["1-TO-1 TUTORING & DOUBT SOLVING",3000,""]}
+                    repeat={Infinity}
+                    // omitDeletionAnimation={true}
+                  
+                ></TypeAnimation></div>
+                <Fade>
                 <div className='webPara'>
                     A look at the importance of technology 
                     in doubt solving and learning
                 </div>
+                <div className='webPara'>
+                Students have access to educational resources and support services 24/7. This flexibility allows students to learn at their convenience and seek help whenever they need it, promoting continuous learning and improvement.
+                </div>
+                </Fade>
+                <div className='introButtons'>
+               <Button text={"SignUp"} path={"signup"}></Button>
+               <Button text={"LogIn"} path={"login"}></Button>
+            </div>
             </div>
             <div className='webIntroVideo'>
             <video muted loop autoPlay>
@@ -21,10 +39,15 @@ const Home = () => {
                 </video>
             </div>
             </div>
-            <div className='introButtons'>
+            {/* <div className='introButtons'>
                <Button text={"SignUp"} path={"signup"}></Button>
                <Button text={"LogIn"} path={"login"}></Button>
-            </div>
+            </div> */}
+        </div>
+
+        <div className='section2'>
+            <h1></h1>
+
         </div>
       
     </div>
