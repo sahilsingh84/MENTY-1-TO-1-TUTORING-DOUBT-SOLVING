f0cd0d648422329f370ec34913f6bd7c5f7bc062
Sagar1917118 <112215153@cse.iiitp.ac.in>
2024-03-02 00:12:21 +0530

    faltu changes

--- menty/src/App.js
+++ menty/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Home from './pages/Home'
 import { Route,Routes } from 'react-router-dom'
-import Navbar from './components/common/Navbar'
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
